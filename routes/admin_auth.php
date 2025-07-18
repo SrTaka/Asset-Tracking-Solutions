a96875955098aef8cc979ab0ae9d6e280a0282ec
elijahtakudzwa <elijatmaswera@gmail.com>
2025-07-18 14:59:21 +0200

    E.T 18/07/2025 15:00
    Implemented multi-guard auth system
    tested admin and normal user registration and login
    tested admin and user dashboards

---
+++ routes/admin_auth.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Admin\Auth\AuthenticatedAdminController;
+use App\Http\Controllers\Admin\Auth\RegisteredUserController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->middleware('guest:admin')->group(function () {
+    Route::get('register', [RegisteredUserController::class, 'create'])
+        ->name('admin.register');
+    Route::post('register', [RegisteredUserController::class, 'store']);
+
+    Route::get('login', [AuthenticatedAdminController::class, 'create'])
+        ->name('admin.login');
+    Route::post('login', [AuthenticatedAdminController::class, 'store']);
+
+});
+
+Route::prefix('admin')->middleware('auth:admin')->group(function () {
+    // Admin Dashboard
+        Route::get('dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+
+    Route::post('logout', [AuthenticatedAdminController::class, 'destroy'])
+        ->name('admin.logout');
+});
