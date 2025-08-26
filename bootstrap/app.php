2c0dc7fbb57e410ef8b8fecae5a8564dbc2ef3aa
elijahtakudzwa <elijatmaswera@gmail.com>
2025-08-26 12:55:52 +0200

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,14 +3,17 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         using: function () {
-            require __DIR__.'/../routes/web.php';
-            // Load additional route files
-            require __DIR__.'/../routes/auth.php';
-            require __DIR__.'/../routes/admin_auth.php';
+            Route::middleware('web')->group(function () {
+                require __DIR__.'/../routes/web.php';
+                // Load additional route files under the web middleware stack
+                require __DIR__.'/../routes/auth.php';
+                require __DIR__.'/../routes/admin_auth.php';
+            });
         },
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
